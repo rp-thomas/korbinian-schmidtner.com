2ef7a78522399c7d3534ce0d936a81e0a0d0eb62
rp-thomas <thomas.taubmann.dev@icloud.com>
2026-07-13 11:20:08 +0200

    create sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://korbinian-schmidtner.com',
   output: 'server',
   adapter: vercel({
     imageService: true,
   }),
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
